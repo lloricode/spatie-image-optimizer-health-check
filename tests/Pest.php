7d3d819424d0ae9dc44d6f1906d5df537e143711
Lloric Mayuga Garcia <lloricode@gmail.com>
2026-06-22 09:51:45 +0800

    Prevent stray processes in test setup with `Process::preventStrayProcesses()`

--- tests/Pest.php
+++ tests/Pest.php
@@ -2,7 +2,11 @@
 
 declare(strict_types=1);
 
+use Illuminate\Support\Facades\Process;
 use Lloricode\SpatieImageOptimizerHealthCheck\Tests\TestCase;
 
 uses(TestCase::class)
-    ->in(__DIR__);
+    ->in(__DIR__)
+    ->beforeEach(function () {
+        Process::preventStrayProcesses();
+    });
